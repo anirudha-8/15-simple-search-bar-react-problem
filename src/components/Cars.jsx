3fe5db68f20d97070d08e98771cbdb9886771793
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-02 11:02:48 +0530

    add use state for search car brands

--- src/components/Cars.jsx
+++ src/components/Cars.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
-const Cars = ({carBrands}) => {
-    return (
+const Cars = ({ carBrands }) => {
+	const [searchCarBrand, setSearchCarBrand] = useState("");
 
-    )
+	return <div>Cars</div>;
 };
 export default Cars;
