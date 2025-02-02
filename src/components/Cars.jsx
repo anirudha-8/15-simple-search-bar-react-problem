630f5fbf445afafd76b68115b2f500e62082d3fc
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-02 11:05:54 +0530

    create search bar

--- src/components/Cars.jsx
+++ src/components/Cars.jsx
@@ -3,6 +3,19 @@ import { useState } from "react";
 const Cars = ({ carBrands }) => {
 	const [searchCarBrand, setSearchCarBrand] = useState("");
 
-	return <div>Cars</div>;
+	return (
+		<div>
+			<h1>Car Brands</h1>
+			<input
+				type="text"
+				name="carBrand"
+				id="carBrand"
+				placeholder="search car brand..."
+				value={searchCarBrand}
+				onChange={(e) => setSearchCarBrand(e.target.value)}
+			/>
+			<br />
+		</div>
+	);
 };
 export default Cars;
