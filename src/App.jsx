722d7c9413a6871f8ff126a388a6a1ad9dd2c98e
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-02 10:59:23 +0530

    create Car brands list

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import './App.css'
+import "./App.css";
+import Cars from "./components/Cars";
 
 function App() {
+	const carBrands = [
+		"Toyota",
+		"Suzuki",
+		"Audi",
+		"Tata",
+		"Tesla",
+		"BMW",
+		"Porche",
+		"Benz",
+	];
 
-    return ()
+	return (
+		<div>
+			<Cars carBrands={carBrands} />
+		</div>
+	);
 }
 
-export default App
+export default App;
