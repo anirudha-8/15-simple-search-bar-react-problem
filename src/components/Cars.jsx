c7af711afdba94622b537483373c48216ad2ed60
Anirudha Bele <beleanirudha8@gmail.com>
2025-02-03 11:08:16 +0530

    create search function to serach car brands

--- src/components/Cars.jsx
+++ src/components/Cars.jsx
@@ -3,6 +3,10 @@ import { useState } from "react";
 const Cars = ({ carBrands }) => {
 	const [searchCarBrand, setSearchCarBrand] = useState("");
 
+	const filterCarBrands = carBrands.filter((carBrand) => {
+		carBrand.toLowerCase().include(searchCarBrand.toLowerCase());
+	});
+
 	return (
 		<div>
 			<h1>Car Brands</h1>
